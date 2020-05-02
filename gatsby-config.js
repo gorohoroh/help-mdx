69aab5430a62ed5dc12cd85d5b8a22d7f1df301b
gorohoroh <gorohoroh@gmail.com>
2020-05-02 20:43:11 +0300

    MDX config + test page

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,14 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve(`./src/components/layout.js`)
+        }
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
