f44db709a8ce0277e7b2c183ecf08c1bdaa1b193
gorohoroh <gorohoroh@gmail.com>
2020-05-02 23:39:49 +0300

    Source filesystem plugin for pages, mock frontmatter (date, path) in sample page, index.js with an allMdx query to list links to mdx files, and a minor layout update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,48 @@
 import React from "react"
+import { graphql } from "gatsby"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+const IndexPage = ({
+  data: {
+    allMdx: { edges },
+  },
+}) => {
+  const Posts = edges.map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+  return <Layout>
+    <SEO title="Home"/>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div>{Posts}</div>
   </Layout>
-)
+}
 
 export default IndexPage
+
+export const PostLink = ({ post }) => (
+  <div>
+    <Link to={post.frontmatter.path}>
+      {post.frontmatter.title} ({post.frontmatter.date})
+    </Link>
+  </div>
+)
+
+export const pageQuery = graphql`
+    query MyQuery {
+      allMdx {
+        edges {
+          node {
+            id
+            excerpt
+            frontmatter {
+              title
+              path
+              date
+            }
+          }
+        }
+      }
+    }
+`
